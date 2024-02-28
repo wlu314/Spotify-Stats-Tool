4f3c1ef0490711798f8c1c6a389ead6db8dee9ea
wlu314 <125327127+wlu314@users.noreply.github.com>
2024-02-28 13:55:17 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CS2340_Project_2
+Spotify Wrapped Stats
---
+++ app/src/main/java/com/example/code/splash_page.java
@@ -0,0 +1,14 @@
+package com.example.code;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class splash_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_page);
+    }
+}
