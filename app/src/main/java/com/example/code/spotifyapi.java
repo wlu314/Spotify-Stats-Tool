bb98d4a9e310052ede1cdc43e4bc67106f604a12
wlu314 <125327127+wlu314@users.noreply.github.com>
2024-03-02 13:41:27 -0500

    Update README.md

--- app/src/main/java/com/example/code/spotifyapi.java
+++ app/src/main/java/com/example/code/spotifyapi.java
@@ -11,4 +11,20 @@ public class spotifyapi extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spotifyapi);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // We will start writing our code here.
+    }
+
+    private void connected() {
+        // Then we will write some more code here.
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Aaand we will finish off here.
+    }
 }
