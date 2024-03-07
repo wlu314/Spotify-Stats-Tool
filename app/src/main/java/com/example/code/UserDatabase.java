eb6facd5622951b94c939c24a87e7e6139587195
Chungman Kim <126294580+Serenata27@users.noreply.github.com>
2024-03-06 23:22:16 -0500

    linked with firebase icloud bucket

--- app/src/main/java/com/example/code/UserDatabase.java
+++ app/src/main/java/com/example/code/UserDatabase.java
@@ -1,4 +1,16 @@
 package com.example.code;
 
-public class UserDatabase {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class UserDatabase extends AppCompatActivity {
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+
+    // Create a storage reference from our app
+    StorageReference storageRef = storage.getReference();
 }
