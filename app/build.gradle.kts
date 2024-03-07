02832825d62ebab10ee78e699a74e92b47e6c8ba
Chungman Kim <126294580+Serenata27@users.noreply.github.com>
2024-03-07 09:14:47 -0500

    continue backEnd Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,14 +43,11 @@ dependencies {
     implementation("com.spotify.android:auth:2.1.1") // Spotify API
     implementation("com.squareup.okhttp3:okhttp:4.9.3") // HTTP Authentication
     implementation("androidx.cardview:cardview:1.0.0") // Card-Based Layout
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-storage:20.3.0")
+    implementation("com.google.firebase:firebase-firestore:24.10.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation(platform("com.google.firebase:firebase-bom:32.7.3"))
-
 }
--- app/src/main/java/com/example/code/UserDatabase.java
+++ app/src/main/java/com/example/code/UserDatabase.java
@@ -1,16 +1,71 @@
 package com.example.code;
 
-import android.os.Bundle;
+import static android.content.ContentValues.TAG;
 
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-public class UserDatabase extends AppCompatActivity {
-    FirebaseStorage storage = FirebaseStorage.getInstance();
+import java.util.HashMap;
+import java.util.Map;
 
-    // Create a storage reference from our app
-    StorageReference storageRef = storage.getReference();
+
+public class UserDatabase extends AppCompatActivity{
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+
+        //how to store data
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+
+        //read Data
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+
+    }
 }
