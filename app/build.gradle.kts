6d829d548b87e54f32898a86bef2b05e3d10dab7
Chungman Kim <126294580+Serenata27@users.noreply.github.com>
2024-03-13 00:35:54 -0400

    token and UID automatically save to DB when connect to Spotify API. Can get data using getToken()

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,11 +54,11 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.navigation:navigation-fragment:2.7.7")
     implementation("androidx.navigation:navigation-ui:2.7.7")
+    implementation("com.google.firebase:firebase-database:20.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(platform("com.google.firebase:firebase-bom:32.7.3"))
     implementation("com.google.firebase:firebase-auth:22.3.1")
-
     implementation("androidx.browser:browser:1.7.0")
 }
