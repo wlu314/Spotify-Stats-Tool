fc8428f6d5c9c5ea8c091d034f63d98f8fd54feb
curtis03 <curtiswencao@gmail.com>
2024-03-28 15:00:04 -0400

    Endpoints Fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,10 @@ repositories {
 }
 
 dependencies {
-
+    implementation ("com.google.code.gson:gson:2.8.6")
+    implementation ("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     implementation("com.spotify.android:auth:2.1.1") // Spotify API
     implementation("com.squareup.okhttp3:okhttp:4.9.3") // HTTP Authentication
     implementation("androidx.cardview:cardview:1.0.0") // Card-Based Layout
